55a6cebcc9d06d868390b563e1a10ea791fff954
Gonzalo <admin@powerlinedesign.es>
2023-10-13 21:14:17 +0200

    Actualizacion de estilos

--- index.php
+++ index.php
@@ -1,43 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <meta name="robots" content="noindex, nofollow">
-    <title>Calculador de precio</title>
+    <title>Calculadora de Precios</title>
 </head>
+
 <body>
 
-<div class="container">
-    <h1>Calculador de precio</h1>
+    <div class="container">
+        <div class="logo">
+            <img src="https://www.bluenty.com/media/menus/submenu/icon/bluenty-logo.webp" alt="Bluenty Logo">
+        </div>
+        <h1>Calculadora de Precios</h1>
 
-    <form method="POST" action="">
-        <label for="precio">Precio recomendado:</label>
-        <input type="text" name="precio" required><br>
+        <section class="information">
+            <h2>Información de IVA</h2>
+            <p>Gafas graduadas: 10%</p>
+            <p>Gafas de sol: 21%</p>
+            <p>Gafas Deportivas: 10%</p>
+            <p>Lentillas: 10%</p>
+        </section>
 
-        <label for="iva">IVA (%):</label>
-        <input type="text" name="iva" required><br>
+        <section class="calculator">
+            <h2>Calculadora</h2>
+            <form method="POST" action="">
+                <label for="precio">Precio recomendado:</label>
+                <input type="text" name="precio" required><br>
 
-        <input type="submit" name="submit" value="Calcular">
-    </form>
+                <label for="iva">IVA (%):</label>
+                <input type="text" name="iva" required><br>
 
-    <?php
-    if (isset($_POST['submit'])) {
-        $precio = $_POST['precio'];
-        $iva = $_POST['iva'];
+                <input type="submit" name="submit" value="Calcular">
+            </form>
 
-        // Descontar el 30%
-        $precio_descontado = $precio * 0.70;
+            <?php
+            if (isset($_POST['submit'])) {
+                $precio = $_POST['precio'];
+                $iva = $_POST['iva'] / 100;
 
-        // Restar el IVA
-        $precio_final = $precio_descontado / (1 + $iva / 100);
+                // Descontar el 30%
+                $precio_descontado = $precio * 0.70;
 
-        echo "<div class='result'>El precio final es: " . number_format($precio_final, 2) . " €</div>";
-    }
-    ?>
-</div>
+                // Aplicar el IVA
+                $precio_final = $precio_descontado * (1 + $iva);
+
+                echo "<div class='result'>El precio final es: " . number_format($precio_final, 2) . " €</div>";
+            }
+            ?>
+        </section>
+    </div>
 
 </body>
-</html>
 
+</html>
